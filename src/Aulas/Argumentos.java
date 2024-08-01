f24c678a89fb17d400884d9d3b5e9e8660b64e5e
André Rocha <andrenakarocha@hotmail.com>
2024-08-01 12:21:06 -0300

    Aula Argumentos

--- src/Aulas/Argumentos.java
+++ src/Aulas/Argumentos.java
@@ -2,6 +2,11 @@ package Aulas;
 
 public class Argumentos {
     public static void main(String[] args){
-
+        // Nessa situação, devemos passar o argumento ao executar o programa usando um terminal
+        // Ao passar o argumento, podemos manipulá-lo da forma que desejarmos
+        // Usando esse formato, só podemos passar os argumentos pelo TERMINAL ao executar o código:
+        // $ java Argumentos.java André
+        // java -> Usado para rodar o arquivo | Argumentos.java -> Nome do arquivo | André -> O argumento passado
+        System.out.println("Olá " + args[0]);
     }
 }
