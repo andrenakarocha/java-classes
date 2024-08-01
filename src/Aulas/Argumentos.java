d1ca3834f3a906951a34adae633e47be7f1bceaf
André Rocha <andrenakarocha@hotmail.com>
2024-08-01 11:40:07 -0300

    Aula Funções

---
+++ src/Aulas/Argumentos.java
@@ -0,0 +1,7 @@
+package Aulas;
+
+public class Argumentos {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/Aulas/Funções.java
@@ -0,0 +1,19 @@
+package Aulas;
+
+public class Funções {
+
+    public static void main(String[] args){
+        sum(10, 20);
+    }
+
+    // static void -> Definição padrão
+    // sum -> Nome da função
+    // () -> Parâmetros
+    // {} -> Bloco a ser executado
+    static void sum(int x, int y){
+        System.out.println(x + y);
+    }
+
+}
+
+
