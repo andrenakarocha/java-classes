298332f079aef9374b690367cf29625ba9daabb9
André Rocha <andrenakarocha@hotmail.com>
2024-08-01 13:21:55 -0300

    Aula Loops

---
+++ src/Aulas/Loops.java
@@ -0,0 +1,23 @@
+package Aulas;
+
+// Criando números aleatórios com LOOP
+import java.util.Random;
+
+public class Loops {
+    public static void main(String[] args) {
+        var generate = new Random();
+
+        var i = 0;
+        while (i < 6) {
+            var number = generate.nextInt(60);
+            System.out.println(number);
+            i++;
+        }
+
+//        Mesma lógica, usando o for
+//        for (var i = 0; i < 6; i++){
+//            var number = generate.nextInt(60);
+//            System.out.println(number);
+//        }
+    }
+}
