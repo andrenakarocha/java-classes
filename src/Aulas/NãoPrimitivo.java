1cdf76c4ebfe801e02e2f58cb449cbe025d286e6
André Rocha <andrenakarocha@hotmail.com>
2024-08-02 10:42:46 -0300

    Aula Tipos Não Primitivos

---
+++ src/Aulas/NãoPrimitivo.java
@@ -0,0 +1,11 @@
+package Aulas;
+
+public class NãoPrimitivo {
+    public static void main(String[] args) {
+        int x = 2;
+        // O Integer é uma class, ou seja, posso fazer vários tipos de operações com o número, diferentemente de usar um tipo primitivo normal.
+        Integer y = 4;
+        // Neste caso, estou transformando o número 4 para uma string "4". Você pode fazer muitas outras operações usando o Integer.
+        y.toString();
+    }
+}
