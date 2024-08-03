697b198a6be2af602f85186535f13a75d89dcb14
André Rocha <andrenakarocha@hotmail.com>
2024-08-03 09:40:11 -0300

    Aula Tipos Primitivos

---
+++ src/Fundamentos/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package Fundamentos;
+
+public class TiposPrimitivos {
+
+    int i = 1;
+    // Booleano, True or False
+    boolean isLogged = true;
+    // Usado para alguns cálculos específicos, mexer com imagens e etc...
+    byte b = 'a';
+    // Diferente de uma string, apenas um caractér
+    char c = 'c';
+    // = int (só com menos espaços, números pequenos)
+    short s = -129;
+    // = int (só com mais espaços, números grandes)
+    long l = 910831983;
+    // 32bit (menor que o double) tem que usar o 'f' para mostrar o ponto flutuante
+    float f = 19.19f;
+    // 64bit
+    double d = 19.19;
+}
