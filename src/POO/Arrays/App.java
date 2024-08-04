9a1902c4367b32ddc24afee7ebab24ed96c63604
André Rocha <andrenakarocha@hotmail.com>
2024-08-04 17:31:21 -0300

    Aula Arrays POO

---
+++ src/POO/Arrays/App.java
@@ -0,0 +1,20 @@
+package POO.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Isso é uma array de 10 users
+        User[] users = new User[10];
+
+        // Aqui usamos o for para criar um loop e criar esses 10 usuários
+        for (int i = 0; i < users.length; i++){
+            User actual = new User();
+            actual.setFirstName("Nome " + i);
+            actual.setLastName("Sobrenome " + i);
+            users[i] = actual;
+        }
+
+        System.out.println(users[2].getFirstName());
+        System.out.println(users[2].getLastName());
+    }
+}
---
+++ src/POO/Arrays/User.java
@@ -0,0 +1,24 @@
+package POO.Arrays;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
