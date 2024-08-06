f563edfe8f8329db3711b3d5203f85c240c72fd1
André Rocha <andrenakarocha@hotmail.com>
2024-08-05 21:58:05 -0300

    Aula Ordenação de Listas

---
+++ src/Fundamentos/Ordenação.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+import java.util.Arrays;
+
+public class Ordenação {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{10, 30, 20, 40, 60, 120, 869, 39009, 100};
+        Arrays.sort(numbers);
+
+        // Quando você printa direto, ele imprime o hashcode, não a lista real.
+        System.out.println(numbers);
+
+        // Agora sim, ele imprime o objeto convertido para uma String.
+        System.out.println(Arrays.toString(numbers));
+    }
+}
