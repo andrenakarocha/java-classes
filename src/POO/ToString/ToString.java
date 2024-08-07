aee01bdf5169248366a8134bc6c14ea7dc0b0429
André Rocha <andrenakarocha@hotmail.com>
2024-08-07 10:50:09 -0300

    Aula Método ToString

---
+++ src/POO/ToString/ToString.java
@@ -0,0 +1,14 @@
+package POO.ToString;
+
+import POO.ToString.User;
+
+public class ToString {
+    public static void main(String[] args) {
+        // Usei o user de uma aula de POO para não criar outro
+        User userA = new User("André", "Rocha");
+
+        // Todos os objetos possuem uma função toString()
+        // Aqui a função já está formatada da minha forma:
+        System.out.println(userA.toString());
+    }
+}
---
+++ src/POO/ToString/User.java
@@ -0,0 +1,35 @@
+package POO.ToString;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    // Esse é o Construtor, o método que executa primeiro quando um objeto "ganha vida"
+    // Ao usar o contrutor, podemos já definir propriedades padrões na criação do objeto, não precisando usar os Setters
+    public User(String firstName, String lastName) {
+        this.firstName = firstName.toUpperCase();
+        this.lastName = lastName.toUpperCase();
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName.toUpperCase();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName.toUpperCase();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Aqui eu posso mudar o método toString da forma que eu quiser, por exemplo, imprimindo a estrutura do User:
+    public String toString() {
+        return "User {" + "firstName=" + firstName + "," + "lastName=" + lastName + "}";
+    }
+}
