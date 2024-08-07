a853a897990a7271d948bc6a6aa4d861721512a7
André Rocha <andrenakarocha@hotmail.com>
2024-08-07 11:01:22 -0300

    Aula Hascode E Equals

---
+++ src/POO/HashcodeAndEquals/App.java
@@ -0,0 +1,12 @@
+package POO.HashcodeAndEquals;
+
+public class App {
+    public static void main(String[] args) {
+        User userA = new User("André", "Rocha");
+        User userB = new User("André", "Rocha");
+
+        // Isso será false, por que mesmo sendo iguais, o equals compara os Hashcodes
+        // Porém com os métodos sobrescrevidos no user, ele da verdadeiro, comparando as propriedades.
+        System.out.println(userA.equals(userB));
+    }
+}
---
+++ src/POO/HashcodeAndEquals/User.java
@@ -0,0 +1,47 @@
+package POO.HashcodeAndEquals;
+
+import java.util.Objects;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    // Esse é o Construtor, o método que executa primeiro quando um objeto "ganha vida"
+    // Ao usar o contrutor, podemos já definir propriedades padrões na criação do objeto, não precisando usar os Setters
+    public User(String firstName, String lastName) {
+        this.firstName = firstName.toUpperCase();
+        this.lastName = lastName.toUpperCase();
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName.toUpperCase();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName.toUpperCase();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Aqui eu apertei "shift + shift" e ele sobrescreveu os métodos.
+    // Agora ele compara as propriedades, não os hashcodes
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
